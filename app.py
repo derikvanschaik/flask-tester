d4bb36caffcb5edf403686ca71bb2b8147197f64
Derik van Schaik <derikvanschaik@gmail.com>
2021-11-27 21:04:04 -0800

    split index.html into two templates: author and authors template.

--- app.py
+++ app.py
@@ -6,20 +6,23 @@ from api_calls import fetch_data, get_authors_and_quotes
 
 # routes 
 @app.route("/", methods=['POST', 'GET'])    
-@app.route("/<author>", methods=['POST', 'GET'])   
-def hello_world(author=None):
+def authors():
     authors, quotes = get_authors_and_quotes() 
-    image_src = None
     # return filtered authors by user search 
     if request.method == 'POST':
         user_search = request.form['author-search']
         filtered_authors = [author for author in authors if author.lower().startswith(user_search) ] 
-        return render_template('index.html', author=author, quotes=quotes,authors=filtered_authors, image_src = image_src)
+        return render_template('authors.html',quotes=quotes,authors=filtered_authors)
 
-    if author:
-        image_src = get_image_from_wiki(author)
+    return render_template('authors.html', quotes=quotes,authors=authors)    
 
-    return render_template('index.html', author=author, quotes=quotes,authors=authors, image_src = image_src)   
+@app.route("/<author>")
+def get_author(author=None): 
+    print("calling author", author) 
+    _, quotes = get_authors_and_quotes() 
+    image_src = get_image_from_wiki(author)
+    return render_template('author.html', quotes= quotes, author=author, image_src = image_src)  
+
 
 if __name__ == '__main__': 
     app.run(debug=True)
